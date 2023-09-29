30926b8f25c9a73fe2858f7e72f11656ffe17084
btran <briantran@cmail.carleton.ca>
2023-09-28 20:47:52 -0400

    Created BuddyInfo info object Created AddressBook object Add and remove buddy.

---
+++ src/BuddyInfo.java
@@ -0,0 +1,31 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private int phone_number;
+
+    public String getName() {
+        return name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public int getPhone_number() {
+        return phone_number;
+    }
+
+    public BuddyInfo(String name, String address, int phone_number) {
+        this.name = name;
+        this.phone_number = phone_number;
+        this.address = address;
+
+    }
+    public BuddyInfo(){
+        name = "default name";
+        this.phone_number = 111;
+        this.address = "default address";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
